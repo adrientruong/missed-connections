bcedf8b06f04521912c7bd57f5c661f225975332
Adrien Truong <adrien.truong@me.com>
2015-01-25 15:12:42 -0800

    remove debug stuff

--- iOS/MissedConnections/LocationController.m
+++ iOS/MissedConnections/LocationController.m
@@ -58,15 +58,6 @@
     NSString *facebookUserID = [[PFUser currentUser] objectForKey:@"fbid"];
     
     [[ServerClient sharedClient] postLocationUpdates:locations forFacebookUserID:facebookUserID];
-    
-    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert categories:nil];
-    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
-    
-    UILocalNotification *notif = [[UILocalNotification alloc] init];
-    notif.fireDate = [NSDate date];
-    notif.alertBody = [notif.fireDate description];
-    [[UIApplication sharedApplication] scheduleLocalNotification:notif];
-    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
 }
 
 @end
