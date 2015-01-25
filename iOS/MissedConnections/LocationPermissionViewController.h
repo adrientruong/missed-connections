3e2f5356b4c15016ac7f15721ea554545bd2dc34
Rohan Shah <rohanyshah@me.com>
2015-01-25 00:16:45 -0800

    Revert f73d2a1..c9e3a34
    This rolls back to commit f73d2a143ad34396bd6c39132b3d606bbd4322f5.

---
+++ iOS/MissedConnections/LocationPermissionViewController.h
@@ -0,0 +1,15 @@
+//
+//  LocationPermissionViewController.h
+//  MissedConnections
+//
+//  Created by Adrien on 1/24/15.
+//  Copyright (c) 2015 Adrien Truong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationPermissionViewController : UIViewController
+
+
+
+@end
