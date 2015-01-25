3175149e9852dd6fb967be5f58b71b9f72dcfee8
Adrien Truong <adrien.truong@me.com>
2015-01-25 09:45:08 -0800

    more friend stuff

---
+++ iOS/MissedConnections/ContactRequestTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ContactRequestTableViewCell.m
+//  MissedConnections
+//
+//  Created by Adrien on 1/25/15.
+//  Copyright (c) 2015 Adrien Truong. All rights reserved.
+//
+
+#import "ContactRequestTableViewCell.h"
+
+@implementation ContactRequestTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
