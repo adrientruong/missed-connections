2f69f255afde6e3607debf4bdb3b19c6ce24de77
Adrien Truong <adrien.truong@me.com>
2015-01-25 04:51:23 -0800

    begin contact requests

---
+++ iOS/MissedConnections/ContactRequestsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactRequestsViewController.h
+//  MissedConnections
+//
+//  Created by Adrien on 1/25/15.
+//  Copyright (c) 2015 Adrien Truong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactRequestsViewController : UIViewController
+
+@end
---
+++ iOS/MissedConnections/ContactRequestsViewController.m
@@ -0,0 +1,37 @@
+//
+//  ContactRequestsViewController.m
+//  MissedConnections
+//
+//  Created by Adrien on 1/25/15.
+//  Copyright (c) 2015 Adrien Truong. All rights reserved.
+//
+
+#import "ContactRequestsViewController.h"
+
+@interface ContactRequestsViewController ()
+
+@end
+
+@implementation ContactRequestsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
